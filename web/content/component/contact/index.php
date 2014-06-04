bce94458d3b33cbc354aea6dc126387c0039b294
aeuxo <terry-nicholls@live.co.uk>
2014-06-04 21:04:24 +0100

    contact/index.php

--- web/content/component/contact/index.php
+++ web/content/component/contact/index.php
@@ -1,10 +1,10 @@
-<?php include'../modules/header.php' ?>
+<?php include'../../modules/header.php' ?>
 
 <div id="mainwindow">
     
     <div id="contacthead">
         
-        <img src="../images/contacthead.png"/>
+        <img src="/images/contacthead.png"/>
         
     </div>
     
@@ -12,25 +12,25 @@
         
         <tr>
             
-            <td><img src="../images/name.png"/></td>
+            <td><img src="/images/name.png"/></td>
             <td><input class="blackinput" type="text" placeholder="enter name here..."/></td>
             
         </tr>
         <tr>
             
-            <td><img src="../images/username.png"/></td>
+            <td><img src="/images/username.png"/></td>
             <td><input class="blackinput" type="text" placeholder="enter username here..."/></td>
             
         </tr>
         <tr>
             
-            <td><img src="../images/email.png"/></td>
+            <td><img src="/images/email.png"/></td>
             <td><input class="blackinput" type="text" placeholder="enter email here..."/></td>
             
         </tr>
         <tr>
             
-            <td style="vertical-align:top;"><img src="../images/message.png"/></td>
+            <td style="vertical-align:top;"><img src="/images/message.png"/></td>
             <td><textarea class="blacktextarea" placeholder="enter message here..."></textarea></td>
             
         </tr>
