dc8b1196a41f7bd3a8517422bcd580c3c673973b
George Doodson <doogeo@googlemail.com>
2014-06-05 00:28:39 +0100

    working on register functions using ajax, jquery and PHP, all contained within the register folder

---
+++ web/content/component/register/ajax.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+      $("#submit").click(function() {
+        var username = $('#username').val();
+        var password = $('#password').val();
+        var confirm = $('#confirm').val();
+        var email = $('#email').val();
+        $.ajax({
+            type: "POST",
+            url: "register.php",
+            data: "username=" + username + "&password=" + password + "&confirm" + confirm + "&email=" + email,
+            success: function(html) {
+                window.location.href = '../../index.php';
+            }
+        });
+        return false;
+
+    });
+})
---
+++ web/content/component/register/register.php
@@ -0,0 +1,16 @@
+<?php
+
+$query=mysql_connect("localhost","root","");
+mysql_select_db("seratron",$query);
+if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']))
+{
+$username=mysql_real_escape_string($_POST['username']);
+$password=mysql_real_escape_string($_POST['password']);
+$email=$_POST['email'];
+$query2=mysql_query("insert into users values('','$username','$password','$email')");
+if($query2)
+{
+echo "<h2>Your Registration Process succesfully completed. Thank You</h2>";
+}
+}
+?>
