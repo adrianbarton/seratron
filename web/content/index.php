e2292f672746672c7754d53f4f52788dfe5cb351
George Doodson <doogeo@googlemail.com>
2014-06-04 21:09:58 +0100

    checking db connection on main server

--- web/content/index.php
+++ web/content/index.php
@@ -1,12 +1,22 @@
-<?php include'modules/header.php' ?>
+<?php 
+include'modules/header.php'; 
+include'lib/db.php';
+?>
 
 <div class="container">
 
     <?php include'modules/leftsidebar.php'?>
     
     <div id="mainwindow">
-
         
+        <?php
+        $db = new db;
+        $select = $db->select_db('seratron');
+        
+        if($select == TRUE){
+            echo "selected database";
+        }
+        ?>
 
     </div>
     
