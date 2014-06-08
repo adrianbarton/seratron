8ec571e1d3bc31692f57fc2dbbdc67ddd960a6c9
George Doodson <doogeo@googlemail.com>
2014-06-08 02:33:29 +0100

    trying to locate file

--- web/content/component/register/checkUsername.php
+++ web/content/component/register/checkUsername.php
@@ -1,13 +1,12 @@
 <?php
- require_once '../../lib/db.php';
+ require_once '/lib/db.php';
  
  $database = new read('seratron', 'users');
  
   $username=$_POST["username"];
   
   $result = $database->values('username', $username);
-  
- 
+   
   $find=mysql_num_rows($result);
  
   echo $find;
