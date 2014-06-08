ef8acbbc09d4de970d18082a30afdf531b9444a4
George Doodson <doogeo@googlemail.com>
2014-06-08 04:50:11 +0100

    trying to fix username problem

--- web/content/component/register/checkUsername.php
+++ web/content/component/register/checkUsername.php
@@ -1,12 +1,11 @@
 <?php
- require_once '/lib/db.php';
+ require_once '../../lib/db.php';
  
  $database = new read('seratron', 'users');
  
   $username=$_POST["username"];
   
-  $result = $database->values('username', $username);
-   
+  $result = $database->values(array('username'=>$username));
   $count = count($result);
  
   echo $count;
--- web/content/component/register/register.php
+++ web/content/component/register/register.php
@@ -3,15 +3,10 @@
 $query=mysql_connect("localhost","root","");
 mysql_select_db("seratron",$query);
 if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']))
-
-$connect=new insert("seratron", "users");
-if(isset($_POST['username']) && isset($_POST['password']))
 {
-$connect->values(array('username'=>$_POST['username'], 'password'=>$_POST['password']));
-
-if($query2)
-{
-echo "<h2>Your Registration Process succesfully completed. Thank You</h2>";
-}
+$username=mysql_real_escape_string($_POST['username']);
+$password=mysql_real_escape_string($_POST['password']);
+$email=$_POST['email'];
+$query2=mysql_query("insert into users values('','$username','$password','$email')");
 }
 ?>
