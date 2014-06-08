1c095a3ef107058e405d58b6588d0c616c646d8a
George Doodson <doogeo@googlemail.com>
2014-06-08 04:31:08 +0100

    oh terry why are you being like this

--- web/content/component/register/register.php
+++ web/content/component/register/register.php
@@ -8,7 +8,7 @@ $connect=new insert("seratron", "users");
 if(isset($_POST['username']) && isset($_POST['password']))
 {
 $connect->values(array('username'=>$_POST['username'], 'password'=>$_POST['password']));
-var_dump($connect);
+
 if($query2)
 {
 echo "<h2>Your Registration Process succesfully completed. Thank You</h2>";
