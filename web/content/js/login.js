9e87facd30b574c2054a283e37d98ccd2738de3d
aeuxo <terry-nicholls@live.co.uk>
2014-06-08 20:39:19 +0100

    sdfdfdfgds hg sdg

--- web/content/js/login.js
+++ web/content/js/login.js
@@ -1,4 +1,4 @@
-
+/* georges 
     $(document).ready(function() {
         
         $(document).on("click", ".login", function() {
@@ -15,4 +15,30 @@
                     };
                 });
         
+    });
+    */
+    
+  
+
+    $(document).ready(function() {
+
+        function processForm(formId) {
+            $.post("ajax/login.php", $("#" + formId).serialize(), function(data) {
+                
+                if (typeof data.empty != "undefined") {
+                    $("#message").html(data.empty);
+                } else if (typeof data.invalid != "undefined") {
+                    $("#message").html(data.invalid);
+                } else if (typeof data.valid != "undefined") {
+                    window.location.href = '/overview';
+                }
+                
+            }, 'json');
+        }
+
+        $('.loginbut').on("click", function() {
+            console.log("CLICK");
+            processForm('loginform');
+        })
+
     });
---
+++ web/content/modules/register.php
@@ -0,0 +1,60 @@
+<?php include'component/register/register.php'; ?>
+
+<script src="/js/register/checkUsername.js"></script>
+<script src="/js/register/passwordStrength.js"></script>
+<script src="/js/register/passwordMatch.js"></script>
+<div id="register" data-reg="deselected">
+
+        R<br/>E<br/>G<br/>I<br/>S<br/>T<br/>E<br/>R
+
+    </div>
+    <div id="regformcontainer" class="hidden">
+
+        <div id="registerform">
+
+            <form name="form" id="form" method="post">
+
+                <table class="registertable">
+
+
+
+                    <tr>
+
+                        <td>username :</td>
+                        <td><input type="text" id="username" name="username"/></td>
+                        <td><div id="usernameCheck"></div></td>
+
+                    </tr>
+                    <tr>
+
+                        <td>email :</td>
+                        <td><input type="text" id="email" name="email" /></td>
+
+                    </tr>
+                    <tr>
+
+                        <td >password :</td>
+                        <td><input type="password" id="password" name="password"/></td>
+                        <td><div id="passwordStrengthCheck"></div></td>
+
+                    </tr>
+                    <tr>
+
+                        <td >confirm password :</td>
+                        <td><input type="password" id="confirm" name="confirm"/></td>
+                        <td><div id="passwordMatchCheck"></div></td>
+
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td style="text-align:left;"><input id="submit" type="submit" name="submit" value="register"/></td>
+
+                    </tr>
+
+                </table>   
+
+            </form>
+
+        </div>
+
+    </div>
