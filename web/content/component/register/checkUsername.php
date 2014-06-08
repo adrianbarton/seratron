008e3bc9dcf47c5c0f2b33bf3ad64f86fffe656a
aeuxo <terry-nicholls@live.co.uk>
2014-06-08 04:15:20 +0100

    register/checkUsername.php

--- web/content/component/register/checkUsername.php
+++ web/content/component/register/checkUsername.php
@@ -1,10 +1,5 @@
 <?php
-<<<<<<< HEAD
-
  require_once '/lib/db.php';
-=======
- require_once '../../lib/db.php';
->>>>>>> 58a5840f822f03a7c83063e143e24d9d44a78f1d
  
  $database = new read('seratron', 'users');
  
--- web/content/component/register/register.php
+++ web/content/component/register/register.php
@@ -1,13 +1,11 @@
 <?php
-<<<<<<< HEAD
 
 $query=mysql_connect("localhost","root","");
 mysql_select_db("seratron",$query);
 if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']))
-=======
+
 $connect=new insert("seratron", "users");
 if(isset($_POST['username']) && isset($_POST['password']))
->>>>>>> f7c1d1c00da005ab1ddd3be95d28d6017e638db3
 {
 $connect->values(array('username'=>$_POST['username'], 'password'=>$_POST['password']));
 var_dump($connect);
